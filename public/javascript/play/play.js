d2ad98f419c7309e55d57d1677daf7a55ca2dec5
Matt Rayner <me@mattihew.com>
2017-12-17 19:05:42 +0000

    added tabs to switch between your board and enemy board

--- public/javascript/play/play.js
+++ public/javascript/play/play.js
@@ -2,6 +2,13 @@ var play =
 {
     init: function()
     {
+        $('ul.nav-tabs li a').click(function(e)
+        {
+            e.preventDefault();
+            var target = $(this);
+            target.tab('show');
+            $('table.board').toggleClass('enemy-board', target.text() === 'Enemy Board');
+        });
         const socket = io();
         socket.on('connect', function()
         {
