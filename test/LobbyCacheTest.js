651bda6a42c7d8603bb6bee77d4f6c91cdf0b6bc
Matt Rayner <me@mattihew.com>
2018-01-07 21:06:02 +0000

    added more tests for lobby and lobbycache

--- test/LobbyCacheTest.js
+++ test/LobbyCacheTest.js
@@ -7,8 +7,36 @@ describe('LobbyCache', function()
         it('should return array of all lobbys in cache', function()
         {
             var id = lobbyCache.put();
-            assert.equal(id, lobbyCache.values()[0].id);
+            assert.equal(lobbyCache.values()[0].id, id);
             lobbyCache.remove(id);
+        });
+    });
+    describe('#get()', function()
+    {
+        it('should return a lobby with a particular id', function()
+        {
+            var id = lobbyCache.put();
+            assert.equal(lobbyCache.get(id).id, id);
+            lobbyCache.remove(id);
+        });
+    });
+    describe('#put()', function()
+    {
+        it('should put a lobby into the lobbyCache', function()
+        {
+            var id = lobbyCache.put({board: {}, name: 'testName'});
+            assert.equal(lobbyCache.get(id).name, 'testName');
+            lobbyCache.remove(id);
+        });
+    });
+    describe('#remove()', function()
+    {
+        it('should remove lobby from the lobbyCache', function()
+        {
+            var id = lobbyCache.put();
+            assert.equal(lobbyCache.get(id).id, id);
+            lobbyCache.remove(id);
+            assert.equal(lobbyCache.get(id), undefined);
         });
     });
 });
--- test/LobbyTest.js
+++ test/LobbyTest.js
@@ -1,14 +1,40 @@
 var assert = require('assert');
 var lobbyCache = require('../models/LobbyCache');
+
+var createLobby = function()
+{
+    var id = lobbyCache.put();
+    return lobbyCache.get(id);
+};
 describe('Lobby', function()
 {
-    describe('getTeam', function()
+    describe('#getTeam()', function()
     {
         it('should return a team at the requested index', function()
         {
-            var id = lobbyCache.put();
-            var lobby = lobbyCache.get(id);
-            assert.strictEqual(typeof lobby.getTeam(0).getShipAt === "function", true);
+            var lobby = createLobby();
+            assert.strictEqual(typeof lobby.getTeam(0).getShipAt === 'function', true);
+        });
+    });
+    describe('#getTeamCount()', function()
+    {
+        it('should return the current number of teams', function()
+        {
+            var lobby = createLobby();
+            assert.strictEqual(lobby.getTeamCount(), 0);
+            lobby.getTeam(0);
+            assert.strictEqual(lobby.getTeamCount(), 1);
+            lobby.getTeam(1);
+            assert.strictEqual(lobby.getTeamCount(), 2);
+        });
+    });
+    describe('#getPlayerTeam()', function()
+    {
+        it('should return a team that contains the requested username', function()
+        {
+            var lobby = createLobby();
+            lobby.getTeam(0).players.push('playerName');
+            assert.strictEqual(typeof lobby.getPlayerTeam('playerName').getShipAt === 'function', true);
         });
     });
 });
